e534663fc448d4fdb1ee05c9c6d6cb786494449a
kenwilcox <wilcoxk@gmail.com>
2014-09-14 21:54:33 -0600

    Just the lyrics

--- 99Sodas/ViewController.swift
+++ 99Sodas/ViewController.swift
@@ -13,6 +13,21 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // Just print out the lyrics to the 99 beers song
+    var drink = "soda"
+    for var soda = 99; soda > 0; soda-- {
+      if (soda > 2) {
+        println("\(soda) bottles of \(drink) on the wall, \(soda) bottles of \(drink). Take one down pass it around \(soda - 1) bottles of \(drink) on the wall.")
+      } else {
+        if (soda == 2) {
+          println("\(soda) bottles of \(drink) on the wall, \(soda) bottles of \(drink). Take one down pass it around \(soda - 1) bottle of \(drink) on the wall.")
+        }
+        else if (soda == 1) {
+          println("\(soda) bottle of \(drink) on the wall, \(soda) bottle of \(drink). Take one down pass it around no more bottles of \(drink) on the wall.")
+        }
+      }
+    }
   }
 
   override func didReceiveMemoryWarning() {
