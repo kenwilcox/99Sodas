35580c380b044e818fe504601f7f5f73745bbf12
kenwilcox <wilcoxk@gmail.com>
2014-09-14 22:00:45 -0600

    Added a comment explaining why I did it differently

--- 99Sodas/ViewController.swift
+++ 99Sodas/ViewController.swift
@@ -20,10 +20,12 @@ class ViewController: UIViewController {
       if (soda > 2) {
         println("\(soda) bottles of \(drink) on the wall, \(soda) bottles of \(drink). Take one down pass it around \(soda - 1) bottles of \(drink) on the wall.")
       } else {
+        // Just because I like proper tense
         if (soda == 2) {
           println("\(soda) bottles of \(drink) on the wall, \(soda) bottles of \(drink). Take one down pass it around \(soda - 1) bottle of \(drink) on the wall.")
         }
         else if (soda == 1) {
+          // The song changes for the last verse
           println("\(soda) bottle of \(drink) on the wall, \(soda) bottle of \(drink). Take one down pass it around no more bottles of \(drink) on the wall.")
         }
       }
